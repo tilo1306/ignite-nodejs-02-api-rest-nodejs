a261fd01959364046fb66f05de09a9c666b9dafd
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-03-20 21:36:53 -0300

    refact: add knex router test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './database/database'
 
 const server = fastify()
+
+server.get('/hello', async () => {
+  const table = await knex('sqlite_schema').select('*')
+
+  return table
+})
 
 server.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running!')
