65c1142be3877a8f5e5f98a89764ae59059e0cde
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-03-25 21:07:41 -0300

    test: config ambient test and run test integration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-const server = fastify()
 
-server.register(cookie)
-
-server.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
-
-server.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
 })
